cc2629a3c4f5b03d788f43710454c345b6e576cd
Tiger730 <jakkrit.mo.61@ubu.ac.th>
2020-09-01 21:23:25 +0700

    Updates

---
+++ precache-manifest.b78b07eeb7f0846823bb99346a792bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae0c17920f023293cb4569ed1f14f9d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1a37aa7e86c1282d8179",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a37aa7e86c1282d8179",
+    "url": "/my-app/static/js/main.67e744c7.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
